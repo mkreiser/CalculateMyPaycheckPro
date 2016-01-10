eff8211125fac1d2596bacde612e8dab9fb935a0
mkreiser <mkreise2@illinois.edu>
2016-01-10 18:01:50 -0500

    Add tests

--- test/spec/app.js
+++ test/spec/app.js
@@ -19,4 +19,18 @@ describe('Run', function() {
 			expect($location.path).toHaveBeenCalledWith('some state');
 		});
 	});
+
+	describe('$rootScope.checkLocation', function() {
+		it('should return true if the current path is /settings', function() {
+			$location.path.and.returnValue('/settings');
+
+			expect($rootScope.checkLocation()).toBeTruthy();
+		});
+
+		it('should return false if the current path is not /settings', function() {
+			$location.path.and.returnValue('/anything');
+
+			expect($rootScope.checkLocation()).toBeFalsy();
+		});
+	});
 });
